24a3e70819301ff404b632945c1a8f43bcc52b67
quanghuy2002 <hoangqunghuy753@gmail.com>
2022-01-14 14:43:04 +0700

    bai3

--- src/app/color/color.component.ts
+++ src/app/color/color.component.ts
@@ -8,7 +8,7 @@ import {Img} from '../img';
   styleUrls: ['./color.component.css']
 })
 export class ColorComponent implements OnInit {
-
+  abc3= "violet"
   img : Img = {
     name : ""
   }
@@ -17,7 +17,7 @@ export class ColorComponent implements OnInit {
     this.img.name = 'https://st.quantrimang.com/photos/image/2016/11/11/anh-gif-3.gif'
 // @ts-ignore
     document.getElementById("anh").innerHTML = `
-<button class="btn btn-link " > <a href="http://localhost:64953/"> Back</a></button>
+<button class="btn btn-link " > <a href="http://localhost:4200/"> Back</a></button>
 <img src="${this.img.name}" width="50%">
 `
   }
@@ -26,7 +26,7 @@ export class ColorComponent implements OnInit {
     this.img.name = 'https://media1.giphy.com/media/1gVUhlXhETaRRxzeHO/giphy.gif'
 // @ts-ignore
     document.getElementById("anh").innerHTML = `
-<button class="btn btn-link " > <a href="http://localhost:64953/"> Back</a></button>
+<button class="btn btn-link " > <a href="http://localhost:4200/"> Back</a></button>
 <img src="${this.img.name}" width="50%%">
 `
   }
@@ -35,7 +35,7 @@ export class ColorComponent implements OnInit {
     this.img.name = 'https://i1.wp.com/mir-s3-cdn-cf.behance.net/project_modules/1400/9bc27292880429.5e569ff84e4d0.gif'
 // @ts-ignore
     document.getElementById("anh").innerHTML = `
-<button class="btn btn-link " > <a href="http://localhost:64953/"> Back</a></button>
+<button class="btn btn-link " > <a href="http://localhost:4200/"> Back</a></button>
 <img src="${this.img.name}" width="100%">
 `
   }
@@ -44,5 +44,16 @@ export class ColorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changToBlue(){
+    this.abc3= "blue"
+  }
+
+  changToPink(){
+    this.abc3= "pink"
+  }
+
+  changToYellow(){
+    this.abc3= "yellow"
+  }
 
 }
